e7b7551391e742cc4847cbb2579f1580829326de
ElenaCherpakova <elena.cherpakova@gmail.com>
2021-12-01 02:17:05 +0000

    timer1.js

--- timer1.js
+++ timer1.js
@@ -1,15 +1,20 @@
-let num = process.argv.slice(2);
-let second = Number(num);
-console.log(second);
-const timer = function(second) {
-  if (second === undefined || isNaN(second) || (second <= 0)) {
-    return false;
-  } else {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, second);
+let input = process.argv.slice(2);
+
+const timer = function(arr) {
+  let arr1 = [];
+  let number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    number = parseInt(arr[i]);
+    arr1.push(number);
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] > 0) {
+      setTimeout(()=>{
+        process.stdout.write('\x07');
+      },arr1[i] * 1000);
+    }
   }
 };
-timer(second);
+timer(input);
 
 
